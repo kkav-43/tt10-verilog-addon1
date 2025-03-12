8ec93cb1e1d470658734aa7e31ebaf2063bf4fef
KARTHIK A V <karthik.a.v.2356@gmail.com>
2025-03-12 13:54:22 +0530

    Update test_project.py

--- test/test_project.py
+++ test/test_project.py
@@ -4,16 +4,18 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    dut._log.info("Start")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
 
+    # Reset
     dut.rst_n.value = 0
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    dut._log.info("Reset complete")
 
-    dut.ui_in.value = 3
-    dut.uio_in.value = 4
-    await ClockCycles(dut.clk, 10)
-    assert dut.uo_out.value == 5  # r ≈ 5
+    # Test case
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 50  # Example assertion
